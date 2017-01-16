f31b631b5b6309136f86b631e97f72e1f4e9d9f6
chinmaydd <chinmay1dd@gmail.com>
2017-01-16 10:51:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Sandpile
+
+Sandpile implementation in Rust. WIP.
+
+### Example
+
+```rust
+extern crate sandpile;
+use sandpile::Sandpile;
+
+fn main() {
+    
+}
+```
+
+### License
+
+MIT
---
+++ src/sandpile.rs
@@ -0,0 +1,16 @@
+pub struct Sandpile {
+    dimension: (i32, i32),
+}
+
+impl Add for Sandpile {
+    type Output = Sandpile;
+
+    fn add(self, other: Sandpile) -> Sandpile {
+        
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Sandpile;
+}
