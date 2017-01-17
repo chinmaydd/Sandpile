a59112386659aaad07abfdc59090bef11cbc2a31
chinmaydd <chinmay1dd@gmail.com>
2017-01-17 13:40:26 +0530

    Adds sandpile stabilization code

--- src/sandpile.rs
+++ src/sandpile.rs
@@ -41,14 +41,15 @@ impl Sandpile {
                         }
                     }
 
-                    // #NOYOLO
-                    temp_data.stabilize();
-
-                    Sandpile {
+                    let mut ret = Sandpile {
                         rows: rows,
                         cols: cols,
                         data: temp_data
-                    }
+                    };
+
+                    ret = ret.stabilize();
+
+                    ret
                 }
             }
         }
@@ -62,7 +63,80 @@ impl Sandpile {
         }
     }
 
-    fn stabilize(&mut self) -> &Self {
+    fn topple_pass(mut self, job_queue: Vec<(usize, usize)>) -> Self {
+        let y: usize = (self.cols - 1) as usize;
+        let x: usize = (self.rows - 1) as usize;
+
+        for (i, j) in job_queue {
+            self.data[i][j] = self.data[i][j] - 4;
+            if i == 0 && j == 0 {
+                // Left corner
+                self.data[0][1] += 1;
+                self.data[1][0] += 1;
+            } else if i == 0 && j == y {
+                // Right corner
+                self.data[0][j-1] += 1;
+                self.data[1][j] += 1;
+            } else if i == x && j == 0 {
+                // Bottom left
+                self.data[i-1][0] += 1;
+                self.data[i][1] += 1;
+            } else if i == x && j == y {
+                // Bottom right
+                self.data[i-1][j] += 1;
+                self.data[i][j-1] += 1;
+            } else if i == 0 {
+                // First row
+                self.data[0][j-1] += 1;
+                self.data[0][j+1] += 1;
+                self.data[1][j] += 1;
+            } else if j == 0 {
+                // First collumn
+                self.data[i-1][0] += 1;
+                self.data[i+1][0] += 1;
+                self.data[i][1] += 1;
+            } else if i == x {
+                // Last row
+                self.data[i][j-1] += 1;
+                self.data[i][j+1] += 1;
+                self.data[i-1][j] += 1;
+            } else if j == y {
+                // Last collumn
+                self.data[i-1][j] += 1;
+                self.data[i+1][j] += 1;
+                self.data[i][j-1] += 1;
+            } else {
+                self.data[i-1][j] += 1;
+                self.data[i+1][j] += 1;
+                self.data[i][j-1] += 1;
+                self.data[i][j+1] += 1;
+            }
+        }
+        
+        self
+    }
+
+    fn get_job_queue(&self) -> Vec<(usize, usize)> {
+        let mut job_queue: Vec<(usize, usize)> = Vec::new();
+
+        for i in 0..self.rows as usize {
+            for j in 0..self.cols as usize {
+                if self.data[i][j] > 3 {
+                    job_queue.push((i,j));
+                }
+            }
+        }
+
+        job_queue
+    }
+
+    fn stabilize(mut self) -> Self {
+        let mut job_queue = self.get_job_queue();
+        
+        while !job_queue.is_empty() {
+            self = self.topple_pass(job_queue);
+            job_queue = self.get_job_queue();
+        }
         self
     }
 }
@@ -80,9 +154,7 @@ impl Add for Sandpile {
                 }
             }
 
-            a.stabilize();
-            
-            a
+            a.stabilize()
         } else {
             panic!("The two sandpiles are not compatible for addition!");
         }
@@ -112,6 +184,40 @@ mod tests {
        let b = Sandpile::new(3, 3, Some(vec![1,1,1,1,2,1,2,2,2]));
 
        assert_eq!(a,b)
+    }
+
+    #[test]
+    fn stabilization_test_1() {
+        let a = Sandpile {
+            rows: 3,
+            cols: 3,
+            data: vec![vec![4,4,4], vec![0,0,0], vec![1,1,1]]
+        };
+
+        let b = Sandpile {
+            rows: 3,
+            cols: 3,
+            data: vec![vec![1,2,1], vec![1,1,1], vec![1,1,1]]
+        };
+
+        assert_eq!(a.stabilize(), b);
+    }
+
+    #[test]
+    fn stabilization_test_2() {
+         let a = Sandpile {
+            rows: 3,
+            cols: 3,
+            data: vec![vec![4,4,4], vec![4,4,4], vec![2,2,2]]
+        };
+
+        let b = Sandpile {
+            rows: 3,
+            cols: 3,
+            data: vec![vec![2,3,2], vec![2,3,2], vec![3,3,3]]
+        };
+
+        assert_eq!(a.stabilize(), b);
     }
 
 }
